9bf0b1cb427327cd5a852186f0e290844f73ebcd
Asweh Debrej <18221145@std.stei.itb.ac.id>
2024-02-27 00:12:22 +0700

    restructure + ui changes

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -3,16 +3,45 @@
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
-import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  Button,
+  DropdownTrigger,
+} from "@nextui-org/react";
 import { Link } from "@nextui-org/react";
 import NextLink from "next/link";
 
+import { ChevronDown } from "@/lib/icons";
+
 const pages = [
   { name: "Home", href: "/home" },
-  { name: "Vigenere", href: "/vigenere" },
-  { name: "Vigenere Extended", href: "/vigenere-ext" },
-  { name: "Playfair", href: "/playfair" },
-  { name: "Affine", href: "/affine" },
+  {
+    name: "Chiper",
+    prefix: "/cipher",
+    hrefs: [
+      {
+        name: "Vigenere",
+        href: "/cipher/vigenere",
+      },
+      {
+        name: "Vigenere Extended",
+        href: "/cipher/vigenere-ext",
+      },
+      {
+        name: "Playfair",
+        href: "/cipher/playfair",
+      },
+      {
+        name: "Affine",
+        href: "/cipher/affine",
+      },
+    ],
+  },
   { name: "About", href: "/about" },
 ];
 
@@ -21,7 +50,74 @@ const matchingPrefix = (path, prefix) =>
 
 export default function NavbarComponent({ className }) {
   const location = usePathname();
-  const [active, setActive] = useState(false);
+
+  const renderDropdownButton = (page) => {
+    const buttonClassName = `bg-transparent text-medium ${matchingPrefix(
+      location,
+      page.href
+    ) && "text-primary font-bold"}`;
+
+    return (<DropdownItem
+      key={page.name}
+      href={page.href}
+      aria-current={
+        matchingPrefix(location, page.href) ? "page" : undefined
+      }
+      as={NextLink}
+      className={buttonClassName}>
+      {page.name}
+    </DropdownItem>);
+  };
+
+  const renderPage = (page) => {
+    if (page.hrefs) {
+      const dropdownClassName = `p-0 bg-transparent data-[hover=true]:bg-transparent text-medium ${matchingPrefix(
+        location,
+        page.prefix
+      ) && "text-primary font-bold"}`;
+
+      return (
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className={dropdownClassName}
+                radius="sm"
+                variant="light"
+                color={
+                  matchingPrefix(location, page.href) ? "primary" : "foreground"
+                }
+                endContent=<ChevronDown fill="currentColor" size={16} />>
+                {page.name}
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu>
+            {page.hrefs.map(renderDropdownButton)}
+          </DropdownMenu>
+        </Dropdown>
+      );
+    } else {
+      return (
+        <Link
+          key={page.name}
+          href={page.href}
+          aria-current={
+            matchingPrefix(location, page.href) ? "page" : undefined
+          }
+          color={matchingPrefix(location, page.href) ? "primary" : "foreground"}
+          as={NextLink}>
+          <NavbarItem
+            isActive={location
+              .replace(/\/$/, "")
+              .endsWith(page.href.replace(/\/$/, ""))}>
+            {page.name}
+          </NavbarItem>
+        </Link>
+      );
+    }
+  };
 
   return (
     <Navbar
@@ -29,27 +125,7 @@ export default function NavbarComponent({ className }) {
       classNames={{
         wrapper: "max-w-screen-2xl md:px-8 lg:px-10 xl:px-12",
       }}>
-      <NavbarContent>
-        {pages.map((page) => (
-          <Link
-            key={page.name}
-            href={page.href}
-            aria-current={
-              matchingPrefix(location, page.href) ? "page" : undefined
-            }
-            color={
-              matchingPrefix(location, page.href) ? "primary" : "foreground"
-            }
-            as={NextLink}>
-            <NavbarItem
-              isActive={location
-                .replace(/\/$/, "")
-                .endsWith(page.href.replace(/\/$/, ""))}>
-              {page.name}
-            </NavbarItem>
-          </Link>
-        ))}
-      </NavbarContent>
+      <NavbarContent>{pages.map((page) => renderPage(page))}</NavbarContent>
     </Navbar>
   );
 }
