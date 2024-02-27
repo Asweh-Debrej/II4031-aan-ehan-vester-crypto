9f7a2aa15af63cb19487b03508717ccd87523418
Asweh Debrej <18221145@std.stei.itb.ac.id>
2024-02-27 17:53:25 +0700

    small change

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,7 +1,7 @@
 export default function Layout({ children }) {
   return (
     <main
-      className="flex h-fit flex-col items-center justify-between px-24"
+      className="flex h-fit flex-col items-center justify-between px-2"
     >
       {children}
     </main>
