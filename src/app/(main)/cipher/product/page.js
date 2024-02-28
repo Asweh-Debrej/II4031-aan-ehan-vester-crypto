74894d0d092d37191dd48bb2608af30b0afde384
SirRay03 <81352066+SirRay03@users.noreply.github.com>
2024-02-28 11:12:12 +0700

    Update page.js

--- src/app/(main)/cipher/product/page.js
+++ src/app/(main)/cipher/product/page.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function ProductPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-8 py-4 max-w-5xl w-full">
-      <h1 className="text-4xl font-bold">Vigenere Cipher</h1>
+      <h1 className="text-4xl font-bold">Product Cipher</h1>
       <ProductForm />
     </div>
   );
