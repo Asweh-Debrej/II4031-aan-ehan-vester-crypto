b3f339beeb2049774b6363118f0fcce3e26062dc
Asweh Debrej <18221145@std.stei.itb.ac.id>
2024-02-28 21:08:21 +0700

    at least it's not totally empty :)

--- src/app/(main)/home/page.js
+++ src/app/(main)/home/page.js
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <></>
+    <div className="flex relative items-center justify-center grow h-full">
+      <div className="text-center">
+        <p className="sm:text-lg md:text-4xl font-bold">
+          Still Empty :)
+        </p>
+      </div>
+    </div>
   );
 }
